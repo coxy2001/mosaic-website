49af10b0648d0f08765682401d39553206cb59a6
Nathan <nathan.cox.cb@gmail.com>
2022-08-01 20:08:47 +1200

    SilverStripe + elemental init

---
+++ app/src/Page.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace {
+
+    use SilverStripe\CMS\Model\SiteTree;
+    use Ticker\Model\Company;
+
+    class Page extends SiteTree
+    {
+        private static $db = [];
+
+        private static $has_one = [];
+    }
+}
