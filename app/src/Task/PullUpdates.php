18d34757ef57a86de4477f992fe27384b2f729f8
Nathan <nathan.cox.cb@gmail.com>
2022-08-10 23:38:41 +1200

    PullUpdates echo perms commands

--- app/src/Task/PullUpdates.php
+++ app/src/Task/PullUpdates.php
@@ -20,11 +20,11 @@ class PullUpdates extends BuildTask
         echo shell_exec("cd .. && composer install");
 
         echo "<h3>Running: perms</h3>";
-        shell_exec("cd .. && find . -type d -exec chmod 0775 {} +");
+        echo shell_exec("cd .. && find . -type d -exec chmod 0775 {} +");
         echo "Folders updated <br>";
-        shell_exec("cd .. && find . -type f -exec chmod 0664 {} +");
+        echo shell_exec("cd .. && find . -type f -exec chmod 0664 {} +");
         echo "Files updated <br>";
-        shell_exec("cd .. && chown -R www-data:www-data .");
+        echo shell_exec("cd .. && chown -R www-data:www-data .");
         echo "Ownership updated <br>";
     }
 }
