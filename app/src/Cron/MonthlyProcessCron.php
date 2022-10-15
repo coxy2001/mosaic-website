f283b7468032e6ba446e98eff978ed2ff23cdb7a
David <davidcprince01@gmail.com>
2022-10-15 16:37:24 +1300

    Api-Getter: Cron executes all relevant tasks

--- app/src/Cron/MonthlyProcessCron.php
+++ app/src/Cron/MonthlyProcessCron.php
@@ -2,6 +2,11 @@
 
 namespace Mosaic\Website\Cron;
 
+use Mosaic\Website\Tasks\DeleteCompaniesTask;
+use Mosaic\Website\Tasks\RankCompaniesPETask;
+use Mosaic\Website\Tasks\RankCompaniesROATask;
+use Mosaic\Website\Tasks\UpdateCompaniesTask;
+use Mosaic\Website\Tasks\VersionCompaniesTask;
 use SilverStripe\CronTask\Interfaces\CronTask;
 
 class MonthlyProcessCron implements CronTask
@@ -19,6 +24,10 @@ class MonthlyProcessCron implements CronTask
 
     public function process()
     {
-
+        DeleteCompaniesTask::create()->run(null);
+        UpdateCompaniesTask::create()->run(null);
+        RankCompaniesROATask::create()->run(null);
+        RankCompaniesPETask::create()->run(null);
+        VersionCompaniesTask::create()->run(null);
     }
 }
